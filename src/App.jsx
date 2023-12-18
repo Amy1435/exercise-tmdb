a8a4ffa4091591407c024ace004050de1c0c7547
Amanda <amanda.ausejo@gmail.com>
2023-12-18 16:13:58 +0100

    refactoring app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,3 @@
-// Bonus:
-// - Nel componente SearchBar, aggiungete una select che fa scegliere all'utente tra 'Movie', 'TV Series', 'Person'.
-// All' onSearch, passate quindi DUE parametri (il value dell'input, come nell'esercizio normale,
-// e anche il valore scelto al select). Quindi, effettuare una fetch a una chiamata diversa in base
-//  a che l'utente voglia cercare un film, una serie TVounapersona.
-
 import { useState } from "react";
 import { useEffect } from "react";
 import MovieCard from "./MovieCard";
@@ -15,43 +9,54 @@ function App() {
     const [content, setContent] = useState([]);
     const [contentSearch, setContentSearch] = useState("spiderman");
     const [fetchValue, setFetchValue] = useState("movie");
+    const [error, setError] = useState(); //crea una errore
 
     useEffect(() => {
         const fetchContent = async () => {
-            const response = await fetch(
-                `https://api.themoviedb.org/3/search/${fetchValue}?api_key=${apiKey}&query=${contentSearch}`
-            );
-            const objContent = await response.json();
-            setContent(objContent.results);
+            try {
+                const response = await fetch(
+                    `https://api.themoviedb.org/3/search/${fetchValue}?api_key=${apiKey}&query=${contentSearch}`
+                );
+                const objContent = await response.json();
+                setContent(objContent.results);
+            } catch (error) {
+                console.log(error);
+                setError("Server Error, try again."); ///constrolla gli errori, stampa qualcosa in console e nella pagina
+            }
         };
 
         fetchContent();
     }, [contentSearch, fetchValue]);
 
-    console.log(content);
     const onSearch = async (inputValue, setValue, setSelectSearch) => {
         setContentSearch(inputValue);
         setFetchValue(setSelectSearch);
         setValue("");
     };
-    console.log(`content` + content.overview);
+
     return (
         <>
-            <SearchBar onSearch={onSearch} />
-            <div className="movie-container">
-                {content.map((content) => (
-                    <MovieCard
-                        key={content.id}
-                        title={
-                            fetchValue === "movie"
-                                ? content.title
-                                : content.name
-                        }
-                        poster={content.poster_path}
-                        bio={content.overview}
-                    />
-                ))}
-            </div>
+            {!error && content.length ? (
+                <>
+                    <SearchBar onSearch={onSearch} />
+                    <div className="movie-container">
+                        {content.map((contentItem) => (
+                            <MovieCard
+                                key={contentItem.id}
+                                title={
+                                    fetchValue === "movie"
+                                        ? contentItem.title
+                                        : contentItem.name
+                                }
+                                poster={contentItem.poster_path}
+                                bio={contentItem.overview}
+                            />
+                        ))}
+                    </div>
+                </>
+            ) : (
+                <p>{error ? error : "Loading..."}</p>
+            )}
         </>
     );
 }
